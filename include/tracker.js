7592bf06077331480512171889414ca77e047146
Luna R <snowluna@protonmail.com>
2022-07-20 13:00:15 +0000

    add viewall functions and table expressions

--- include/tracker.js
+++ include/tracker.js
@@ -2,6 +2,7 @@
 // functions for modifying the employees database
 
 const mysql = require("mysql2");
+const { table } = require("table");
 
 const db = mysql.createConnection({
    host: 'localhost',
@@ -10,42 +11,68 @@ const db = mysql.createConnection({
    database: 'employees_test'
 });
 
+function close() {
+   db.end();
+}
+
 function viewAllDepartments()
 {
-   db.query("SELECT * FROM department", (err, results)=> {
+   db.query("SELECT * FROM department;", (err, results)=> {
       if (err) throw err;
-      console.log(results);
+      
+      // Map Data for use by Table
+      let data = results.map(record=>[record.name, record.id])
+      data.unshift(["DEPARTMENT NAME", "ID"]);
+
+      // Print Table
+      console.log(table(data))
    });
 }
 
 function viewAllRoles()
 {
+   db.query("SELECT * FROM role;", (err, results)=> {
+      if (err) throw err;
+      
+      // Map Data for use by Table
+      let data = results.map(record=>[record.title, record.salary, 
+         record.id, record.department_id
+      ])
+      data.unshift(["ROLE TITLE", "SALARY", "ID", "DEPT ID"]);
+
+      // Print Table
+      console.log(table(data))
+   });
 }
 
 function viewAllEmployees()
 {
+   db.query("SELECT * FROM employee;", (err, results)=> {
+      if (err) throw err;
+      
+      // Map Data for use by Table
+      let data = results.map(record=>[record.last_name, record.first_name, 
+         record.id, record.role_id, 
+            record.manager_id ? record.manager_id : "NULL"
+      ])
+      data.unshift(["LAST NAME",  "FIRST NAME", "ID", "ROLE ID", "MANAGER ID"]);
+
+      // Print Table
+      console.log(table(data))
+   });
 }
 
 function addDepartment()
 {
-
 }
 
 function addRole()
 {
-
 }
 
 function addEmployee()
 {
-
 }
 
-function close()
-{
-   db.close();
-}
-
-module.exports = { viewAllDepartments, viewAllRoles, viewAllEmployees, 
-   addDepartment, addRole, addEmployee, close
-}
+module.exports = { viewAllDepartments, close , viewAllRoles, viewAllEmployees, 
+   addDepartment, addRole, addEmployee };
--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const process = require("node:process");
 const fs = require("fs");
 const inquirer = require("inquirer");
-const db = require("mysql2");
-const tracker = require("./include/tracker.js")
+const tracker = require("./include/tracker.js");
 
 const main =()=> {
    tracker.viewAllDepartments();
-   process.exitCode = 0;
-   return;
+   tracker.viewAllRoles();
+   tracker.viewAllEmployees();
+   tracker.close();
+   
+   process.exitCode = 0; return;
 }
 
 main();
