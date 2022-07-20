9639e59e13d8d553fe2611e7202d195fd8124bb5
Luna R <snowluna@protonmail.com>
2022-07-20 11:01:53 +0000

    add sql files

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+USE employees_test;
+
+INSERT INTO department (name) VALUES
+   ('Accounting'),
+   ('IT'),
+   ('Marketing'),
+   ('Design'),
+   ('Management'),
+   ('Human Resources');
+
+INSERT INTO role ( title, salary, department_id ) VALUES
+   ('Accountant', 99000, 1),
+   ('Helpdesk Technician', 10000, 2);
+
+INSERT INTO employee ( first_name, last_name, role_id, manager_id ) VALUES
+   ('Mark', 'Johnson', 1, NULL),
+   ('Alice', 'White', 0, NULL);
