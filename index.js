565a5ee7afe04e257ae85cf7ae70fe1cc8ab9085
Luna R <snowluna@protonmail.com>
2022-07-20 13:17:23 +0000

    start creating inquirer prompt

--- index.js
+++ index.js
@@ -4,11 +4,44 @@ const inquirer = require("inquirer");
 const tracker = require("./include/tracker.js");
 
 const main =()=> {
-   tracker.viewAllDepartments();
-   tracker.viewAllRoles();
-   tracker.viewAllEmployees();
-   tracker.close();
    
+   // Start Prompt
+   inquirer
+      .prompt([
+         {
+            name: "task",
+            type: "list",
+            choices: ["View All Departments",
+               "View All Roles",
+               "View All Employees",
+               "Add A Department",
+               "Add A Role",
+               "Add An Employee"
+            ]
+         }
+
+      ])
+      .then( (answers) => {
+         console.log(answers.task)
+         switch (answers.task)
+         {
+            case "View All Departments":
+               tracker.viewAllDepartments();
+               break;
+            case "View All Roles":
+               tracker.viewAllRoles();
+               break;
+            case "View All Employees":
+               tracker.viewAllEmployees();
+               break;
+            default: 
+               break;
+               return;
+         }
+
+         tracker.close();
+      });
+
    process.exitCode = 0; return;
 }
 
